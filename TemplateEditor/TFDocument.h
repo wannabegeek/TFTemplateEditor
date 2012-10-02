22fcdb18c9ef21f56924a465c086411f7ebebe43
Tom Fewster <tom@wannabegeek.com>
2012-10-02 19:28:00 +0100

    Initial Commit

---
+++ TemplateEditor/TFDocument.h
@@ -0,0 +1,13 @@
+//
+//  TFDocument.h
+//  TemplateEditor
+//
+//  Created by Tom Fewster on 02/10/2012.
+//  Copyright (c) 2012 Tom Fewster. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TFDocument : NSDocument
+
+@end
